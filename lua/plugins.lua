2c60c95805b4db5858ffd61974823f76f1fdbdc1
vladsolokha <vsolokha@gmail.com>
2024-04-17 09:11:10 -0600

    add desc for treesitter context jump

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -384,7 +384,7 @@ return {
       }
       vim.keymap.set('n', '[c', function()
         require('treesitter-context').go_to_context(vim.v.count1)
-      end, { silent = true })
+      end, { silent = true, desc = 'top context' })
     end,
   },
 
