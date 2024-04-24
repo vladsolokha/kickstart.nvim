ee374e853772aec505e8beee948a2139a5dc43c5
vladsolokha <vsolokha@gmail.com>
2024-04-24 11:53:53 -0600

    remove colored symbols

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -462,26 +462,6 @@ return {
 		end,
 	},
 
-	{ -- colored brackets and parentheses and more
-		"p00f/nvim-ts-rainbow",
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup({
-				rainbow = {
-					enable = true,
-					-- list of languages you want to disable the plugin for
-					-- disable = { "jsx", "cpp" }
-					-- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-					extended_mode = true,
-					-- Do not enable for files with more than n lines, int
-					max_file_lines = 1000,
-					-- colors = {}, -- table of hex strings
-					-- termcolors = {} -- table of colour name strings
-				},
-			})
-		end,
-	},
-
 	{ -- leap around a page, hop, use s or S to highlight blocks of code quickly
 		"folke/flash.nvim",
 		event = "VeryLazy",
