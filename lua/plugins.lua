a9345a85741e1f3f59b064c72807b599453bb8b0
vladsolokha <vsolokha@gmail.com>
2024-05-05 00:53:36 -0400

    add colorizer
    for colors displayed on colors highlighting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -707,4 +707,20 @@ return {
     -- swap or exchange stuff horizontally cx on something, move or select, cx again to swap
     { "tommcdo/vim-exchange" },
 
+    { -- color chooser coloring in code
+        'NvChad/nvim-colorizer.lua',
+        config = function()
+            require 'colorizer'.setup({
+                user_default_options = {
+                    RRGGBBAA = true, -- #RRGGBBAA hex codes
+                    AARRGGBB = true, -- 0xAARRGGBB hex codes
+                    rgb_fn = true,   -- CSS rgb() and rgba() functions
+                    hsl_fn = true,   -- CSS hsl() and hsla() functions
+                    css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                    css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                },
+            })
+        end
+    },
+
 }
