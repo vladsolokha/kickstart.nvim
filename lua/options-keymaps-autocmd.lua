492b55bc706167fe05045e2180bf2167045c2f65
Vlad Solokha <vsolokha@ddn.com>
2024-08-06 13:01:29 -0600

    remove unused options
    old options changed

--- lua/options-keymaps-autocmd.lua
+++ lua/options-keymaps-autocmd.lua
@@ -41,30 +41,20 @@ vim.opt.undofile = true
 vim.opt.incsearch = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "no"
 
 -- Decrease update time
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 500
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
--- vim.opt.list = true
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -98,12 +88,11 @@ vim.keymap.set("n", "<leader>z", vim.diagnostic.open_float, { desc = "diag error
 vim.diagnostic.enable(false)
 local is_diag = false
 vim.keymap.set('n', '<leader>x', function()
+    vim.diagnostic.enable(true)
     if is_diag then
-        vim.diagnostic.enable(true)
         vim.diagnostic.show()
         is_diag = not is_diag
     else
-        vim.diagnostic.enable(true)
         vim.diagnostic.hide()
         is_diag = not is_diag
     end
